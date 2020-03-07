4f26d2f3f93f8c2e1194c5ece247ac784c2fc493
Visnu Pitiyanuvath <visnup@gmail.com>
2020-03-07 01:51:06 -0800

    more failure

--- fail.js
+++ fail.js
@@ -1,6 +1,11 @@
 const tape = require("tape");
 
 tape("a failure", t => {
-  t.deepEqual(1, 2, "one is not two");
+  t.deepEqual(1, 2, "one is deeply not two");
+  t.end();
+});
+
+tape("another failure", t => {
+  t.equal(1, 2, "one is not two");
   t.end();
 });
